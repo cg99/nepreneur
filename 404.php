e572ec1fd2611d31aafd0153ba00cccf7f6a38be
cg99 <darkinfinitespace@gmail.com>
2025-07-09 17:41:38 +1000

    Add basic 404 template

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section id="not-found">
+  <h1>Page Not Found</h1>
+  <p>Sorry, the page you are looking for could not be found. You can try searching below or return to the homepage.</p>
+  <?php get_search_form(); ?>
+  <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn">Back to Home</a></p>
+</section>
+
+<?php get_footer(); ?>
+

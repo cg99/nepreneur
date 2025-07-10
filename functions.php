ac56d871656fa651ddf428510c21d2eb9250ded7
cg99 <darkinfinitespace@gmail.com>
2025-07-10 18:52:53 +1000

    is front page for excerpt

--- functions.php
+++ functions.php
@@ -64,7 +64,7 @@ add_action( 'after_switch_theme', 'nep_create_default_pages' );
 
 // Shorten excerpt length for blog listings and archives
 add_filter( 'excerpt_length', function ( $length ) {
-    if ( is_home() || is_archive() ) {
+    if ( is_home() || is_archive() || is_front_page() ) {
         return 20; // limit to 20 words
     }
     return $length;

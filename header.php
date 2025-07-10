17c6de02020bda3b7a82ca96cae9063db1c7950f
cg99 <darkinfinitespace@gmail.com>
2025-07-10 18:18:07 +1000

    Add separate Get Started page

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
         <ul>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
-            <li><a href="#cta">Get Started</a></li>
+            <li><a href="<?php echo esc_url( site_url('/get-started') ); ?>">Get Started</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a href="<?php echo esc_url( get_post_type_archive_link( 'post' ) ); ?>">Blog</a></li>
         </ul>
